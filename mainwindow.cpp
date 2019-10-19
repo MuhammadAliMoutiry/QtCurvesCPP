1f3b5a96ea551c3609f78c7db9237b0dcceab312
Muhammad Ali Moutiry <moutiry4@gmail.com>
2019-10-19 20:44:28 +0200

    Update background and line color update user interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QColorDialog>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -72,4 +72,16 @@ void MainWindow::on_spinInterval_valueChanged(double interval)
 void MainWindow::on_spinCount_valueChanged(int count)
 {
     this->ui->renderArea->setStepCount(count);
+}
+
+void MainWindow::on_btnBackground_clicked()
+{
+    QColor color = QColorDialog::getColor(ui->renderArea->getBackgroundColor(), this, "Select Color");
+    ui->renderArea->setBackgroundColor(color);
+}
+
+void MainWindow::on_btnLineColor_clicked()
+{
+    QColor color = QColorDialog::getColor(ui->renderArea->getShapeColor(), this, "Select Color");
+    ui->renderArea->setShapeColor(color);
 }
